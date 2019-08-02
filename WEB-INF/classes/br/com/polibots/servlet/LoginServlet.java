f4132cd26411584cca7a507893bd0c710daa4205
GuiTadeu <guitadeu.py@gmail.com>
2019-08-01 22:37:32 -0300

    Polibots

---
+++ WEB-INF/classes/br/com/polibots/servlet/LoginServlet.java
@@ -0,0 +1,33 @@
+package br.com.polibots.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String usuario = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		try{
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+
+}
